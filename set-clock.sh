a09f81665699547b2b99e9cbd555311e2c7f63fc
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-14 02:55:10 +0000

    Added port argument to set clock script

--- set-clock.sh
+++ set-clock.sh
@@ -1,6 +1,13 @@
-echo "Setting device clock"
+PORT=$1
+
+if [ ! $PORT ]; then
+  PORT="/dev/ttyUSB0"
+fi
+
+echo "Setting device clock to the current time"
+echo "Device port: $PORT"
 
 DATE_TIME=$(date '+%d/%m/%Y %T');
-echo "$DATE_TIME"
+echo "Clock: $DATE_TIME"
 
-echo "C$DATE_TIME" > /dev/ttyUSB2
+echo "C$DATE_TIME" > "$PORT"
