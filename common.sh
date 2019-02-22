51d896e6572509825badb12be5eee4ba2a40765b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-22 22:25:11 +0000

    Trying to fix auto tests

--- common.sh
+++ common.sh
@@ -1,17 +1,25 @@
 GARDEN_HOSTNAME=garden
 GARDEN_USER=j
 
-IRRIGATOR_PORT="/dev/ttyUSB0"
-SIMULATOR_PORT="/dev/ttyUSB1"
+ILLUMINATOR_PORT=$ILLUMINATOR_PORT
+SIMULATOR_PORT=$ILLUMINATOR_SIMULATOR_PORT
 
-# If multiple devices are detected then this becomes the second device pair
-if pio device list | grep -q 'ttyUSB1'; then
-  IRRIGATOR_PORT="/dev/ttyUSB1"
+if [ ! "ILLUMINATOR_PORT" ]; then
+  ILLUMINATOR_PORT="/dev/ttyUSB0"
+fi
+if [ ! "SIMULATOR_PORT" ]; then
+  SIMULATOR_PORT="/dev/ttyUSB1"
 fi
 
-# If multiple devices pairs are detected then this becomes the second device pair
-if pio device list | grep -q 'ttyUSB2'; then
-  IRRIGATOR_PORT="/dev/ttyUSB2"
-  SIMULATOR_PORT="/dev/ttyUSB3"
-fi
+# TODO: Remove if not needed. Ports should be specified via environment variables
+## If multiple devices are detected then this becomes the second device pair
+#if pio device list | grep -q 'ttyUSB1'; then
+#  ILLUMINATOR_PORT="/dev/ttyUSB1"
+#fi
+
+## If multiple devices pairs are detected then this becomes the second device pair
+#if pio device list | grep -q 'ttyUSB2'; then
+#  VENTILATOR_PORT="/dev/ttyUSB2"
+#  SIMULATOR_PORT="/dev/ttyUSB3"
+#fi
 
