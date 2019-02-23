84775bffdd06c739cd292b9c5a1a768784abc10e
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-23 01:53:13 +0000

    Updated common script

--- common.sh
+++ common.sh
@@ -11,15 +11,3 @@ if [ ! "SIMULATOR_PORT" ]; then
   SIMULATOR_PORT="/dev/ttyUSB1"
 fi
 
-# TODO: Remove if not needed. Ports should be specified via environment variables
-## If multiple devices are detected then this becomes the second device pair
-#if pio device list | grep -q 'ttyUSB1'; then
-#  ILLUMINATOR_PORT="/dev/ttyUSB1"
-#fi
-
-## If multiple devices pairs are detected then this becomes the second device pair
-#if pio device list | grep -q 'ttyUSB2'; then
-#  VENTILATOR_PORT="/dev/ttyUSB2"
-#  SIMULATOR_PORT="/dev/ttyUSB3"
-#fi
-
