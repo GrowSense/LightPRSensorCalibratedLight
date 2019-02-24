fc9b059bf56aa69a82693074adb4fed3f674e572
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-24 06:27:52 +0000

    Trying to fix install platformio

--- install-platformio.sh
+++ install-platformio.sh
@@ -5,7 +5,7 @@ fi
 
 # platform.io
 if ! type "pio" > /dev/null; then
-  sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
+  python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
 fi
 
-sudo platformio upgrade
+platformio upgrade
