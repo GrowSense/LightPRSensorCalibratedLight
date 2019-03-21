3e2fba0b25f601a4fe700b111db98ecec687ca33
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-21 07:55:09 +0000

    Fixed scripts

--- tests/nunit/build.sh
+++ tests/nunit/build.sh
@@ -1,6 +1,10 @@
-echo "Starting build for project"
+echo "Building project tests"
 echo "Dir: $PWD"
 
 DIR=$PWD
 
-xbuild src/LightPRSensorCalibratedLight.sln /p:Configuration=Release
+xbuild src/LightPRSensorCalibratedLight.sln /p:Configuration=Release /verbosity:quiet && \
+
+echo "Finished building project tests." ||
+
+(echo "Failed building project tests!" && exit 1)
