b8fa05e61f6283bfb5e4ab778364caac4ea9da99
CompulsiveCoder <compulsivecoder@gmail.com>
2019-10-27 08:58:13 +1100

    Trying to fix tests

--- tests/nunit/src/LightPRSensorCalibratedLight.Tests.Integration/LightMode.cs
+++ tests/nunit/src/LightPRSensorCalibratedLight.Tests.Integration/LightMode.cs
@@ -2,14 +2,14 @@
 
 namespace LightPRSensorCalibratedLight.Tests.Integration
 {
-	public enum LightMode
-	{
-		Off = 0,
-		On = 1,
-		AboveThreshold = 2,
-		BelowThreshold = 3,
-		Fade = 4,
-		Supplement = 5,
-		Timer = 6
-	}
+  public enum LightMode
+  {
+    Off = 0,
+    On = 1,
+    Timer = 3,
+    AboveThreshold = 4,
+    BelowThreshold = 5,
+    Fade = 6,
+    Supplement = 7
+  }
 }
